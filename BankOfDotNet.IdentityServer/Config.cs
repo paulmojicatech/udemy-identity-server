46c63e49b8ed3fdf0ea2055302f5913c4569f5b2
Paul Mojica <paulmojicatech@gmail.com>
2021-01-03 03:57:01 -0500

    Add configuration for IdentityServer

---
+++ BankOfDotNet.IdentityServer/Config.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using IdentityServer4.Models;
+
+namespace BankOfDotNet.IdentityServer
+{
+    public class Config
+    {
+        public Config()
+        {
+        }
+
+        public static IEnumerable<ApiResource> GetAllApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("bankOfDotNet", "Customer API for BankOfDotNet")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "bankOfDotNet" }
+                }
+            };
+        }
+    }
+}
--- BankOfDotNet.IdentityServer/Startup.cs
+++ BankOfDotNet.IdentityServer/Startup.cs
@@ -16,6 +16,11 @@ namespace BankOfDotNet.IdentityServer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddInMemoryApiResources(Config.GetAllApiResources())
+                .AddInMemoryClients(Config.GetClients());
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,15 +31,7 @@ namespace BankOfDotNet.IdentityServer
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            app.UseIdentityServer();
         }
     }
 }
